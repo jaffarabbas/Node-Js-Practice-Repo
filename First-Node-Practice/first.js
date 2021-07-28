277facb24c4093c460cbc8b8ddd95c62fb7e0ad6
jaffarabbas <gamaportal8@gmail.com>
2021-07-28 22:11:50 +0500

    7-28-2021

---
+++ First-Node-Practice/first.js
@@ -0,0 +1,25 @@
+// console.log('hello')
+const a = 3
+console.log(a)
+// const a = 5
+// console.log(a)
+
+let b = 4
+console.log(b)
+
+
+const check1 = () => {
+    let b = 10;
+    console.log(b)
+}
+
+
+const check2 = () => {
+    const a = 10;
+    console.log(a)
+}
+
+check1();
+console.log(b)
+check2();
+console.log(a);
