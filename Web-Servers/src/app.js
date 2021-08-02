e725f1329f9a51ec8624f25abc55578c0255e1e1
jaffarabbas <gamaportal8@gmail.com>
2021-08-02 20:31:23 +0500

    8-2-2021

--- Web-Servers/src/app.js
+++ Web-Servers/src/app.js
@@ -4,8 +4,16 @@ const path = require('path');
 const app = express();
 
 const dir1 = path.join(__dirname, '../public')
+app.set('view engine', 'hbs');
 
 app.use(express.static(dir1))
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Home',
+        val: true
+    })
+})
 
 app.get('/about' , (req , res) => {
     res.send('Hello about')
