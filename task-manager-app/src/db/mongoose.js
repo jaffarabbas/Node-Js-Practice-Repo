b8e201a91aa5a96bff1f134481673db2a504df1e
jaffarabbas <gamaportal8@gmail.com>
2021-09-02 14:38:49 +0500

    8-2-2021

--- task-manager-app/src/db/mongoose.js
+++ task-manager-app/src/db/mongoose.js
@@ -6,17 +6,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 })
 
 
-const Info = mongoose.model('Info', {
-    description:{
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-    },
-    completed:{
-        type: Boolean,
-        required: true,
-        default: false,
-    }
-})
+
 
