e30193925985e2740fda4d916b9487327fe4fb82
jaffarabbas <gamaportal8@gmail.com>
2021-09-05 12:12:13 +0500

    9-5-2021

--- task-manager-app/src/index.js
+++ task-manager-app/src/index.js
@@ -14,10 +14,8 @@ const bcrypt = require('bcryptjs');
 const CheckPassword = async () => {
     const pass = 'Red1234';
     const hashPassword = await bcrypt.hash(pass, 8);
-    console.log(pass,'\n',hashPassword);
     //checking the password
     const isMatch = await bcrypt.compare(pass, hashPassword);
-    console.log(isMatch)
 }
 
 CheckPassword();
@@ -25,3 +23,17 @@ CheckPassword();
 app.listen(port,()=>{
     console.log('Server start at ',port);
 })
+
+const jwt = require('jsonwebtoken');
+
+//genrating json web token
+
+// const generateToken = async () => { 
+//     const token = jwt.sign({_id: 'abc123'},'thisismytoken' , {expiresIn:'7 days'})
+//     console.log('token:',token);
+
+//     const data = jwt.verify(token, 'thisismytoken')
+//     console.log(data);
+// }
+
+// generateToken();
