1cc1dc112a9bd86daf9860adeca6d68d49335498
jaffarabbas <gamaportal8@gmail.com>
2021-09-05 12:12:19 +0500

    9-5-2021

--- task-manager-app/src/models/user.js
+++ task-manager-app/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -37,8 +38,21 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Invalid password')
             }
         }
-    }
+    },tokens:[{
+        token:{
+            type: String,
+            required: true,
+        }
+    }]
 });
+//checking auth
+userSchema.methods.generateAuthToken = async function(){
+    const user = this;
+    const token = jwt.sign({_id:user._id.toString()},'thisismytoken')
+    user.tokens = user.tokens.concat({token})
+    await user.save(); 
+    return token;
+}
 
 userSchema.statics.findByCredentials = async (email,password) => {
     const user = await User.findOne({email});
