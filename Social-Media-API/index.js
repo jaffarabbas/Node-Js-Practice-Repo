58d72cc7cd87560b2418ac7de40a1cd482084c09
jaffarabbas <gamaportal8@gmail.com>
2021-09-07 18:14:26 +0500

    9-7-2021

---
+++ Social-Media-API/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+//for configring .env files
+dotenv.config();
+//creating object for express class 
+const app = express();
+
+//connection to the database (mongodb atlas)
+mongoose.connect(process.env.MONGO_URL,
+    {useNewUrlParser : true,useUnifiedTopology : true},()=>{
+    console.log('Database Connected');
+})
+
+//middleware
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+//routes
+//user route
+app.use('/api/users',userRouter);
+//auth route
+app.use('/api/auth',authRouter);
+
+app.listen(8800,()=>{
+    console.log('!!!! Local Universe Arrived !!!!\n       !!!Happy Hacking!!!')
+})
