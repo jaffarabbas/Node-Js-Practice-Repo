5cbfcea0c670d24028ddc0e853a92106f2b09328
jaffarabbas <gamaportal8@gmail.com>
2021-09-07 18:14:15 +0500

    9-7-2021

--- Core-Modules-Practice/Practice/promises.js
+++ Core-Modules-Practice/Practice/promises.js
@@ -1,18 +1,20 @@
-const add = (a,b) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            resolve(a+b)
-        },5000)
-    })
-}
+// const add = (a,b) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             resolve(a+b)
+//         },5000)
+//     })
+// }
 
-add(1,3).then(sum => {
-    console.log(sum);
-    add(sum,3).then(sum2 => {
-        console.log(sum2);
-    }).catch(err => {
-        console.log(err);
-    })
-}).catch(err => {
-    console.log(err);
-})
+// add(1,3).then(sum => {
+//     console.log(sum);
+//     add(sum,3).then(sum2 => {
+//         console.log(sum2);
+//     }).catch(err => {
+//         console.log(err);
+//     })
+// }).catch(err => {
+//     console.log(err);
+// })
+
+
