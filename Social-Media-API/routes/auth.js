9f0bafe62158f936a203a096a26944a9123849e2
jaffarabbas <gamaportal8@gmail.com>
2021-09-09 22:11:36 +0500

    9-9-2021

--- Social-Media-API/routes/auth.js
+++ Social-Media-API/routes/auth.js
@@ -3,14 +3,14 @@ const User = require('../models/User');
 
 //Register a new user
 router.post("/register", async (req,res)=>{
+    const newUser = await new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
     try{
-        const user = await new User({
-            username: "jafffar",
-            email: "jafffar@gmail.com",
-            password: "JAFFARempiree"
-        })
-        await user.save();
-        res.send(user);
+        const user = await newUser.save();
+        res.status(200).json(user);
     }
     catch(err){
         res.status(500).send(err);
