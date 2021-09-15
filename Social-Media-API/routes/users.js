29683ac2f326258650db89c88cc63cb6eb32dbf0
jaffarabbas <gamaportal8@gmail.com>
2021-09-15 21:55:45 +0500

    9-15-2021

--- Social-Media-API/routes/users.js
+++ Social-Media-API/routes/users.js
@@ -1,7 +1,33 @@
+const User = require('../models/User');
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
-router.get('/',(req,res)=>{
-    res.send('Users');
+//Update a User 
+router.put("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.user.idAdmin){
+        //for password update only
+        if(req.body.password){
+            try{
+                const salt = await bcrypt.genSalt(8);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            }catch(err){
+                return res.status(500).send(err);
+            }
+        }
+        //for other feilds update
+        try{
+            const UpdateUser = await User.findByIdAndUpdate(req.params.id, {$set: req.body});
+            res.status(200).json("User Updated Successfully!")
+        }catch(err) {
+            return res.status(500).send(err);
+        }
+    }else{
+        return res.status(403).json("You can update only your account")
+    }
 })
+//Delete a User
+//Get a User
+//follow a User
+//Unfollow a User
 
 module.exports = router
