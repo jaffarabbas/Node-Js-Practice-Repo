9cb437030549825e0ca3a64963765884e0e362d2
jaffarabbas <gamaportal8@gmail.com>
2021-09-16 15:26:23 +0500

    6-16-2021

--- Social-Media-API/routes/users.js
+++ Social-Media-API/routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 //Update a User 
 router.put("/:id", async (req, res) => {
-    if(req.body.userId === req.params.id || req.user.idAdmin){
+    if(req.body.userId === req.params.id || req.body.idAdmin){
         //for password update only
         if(req.body.password){
             try{
@@ -26,8 +26,51 @@ router.put("/:id", async (req, res) => {
     }
 })
 //Delete a User
+router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.idAdmin){
+        //for other feilds update
+        try{
+            const DeleteUser = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User Deleted Successfully!")
+        }catch(err) {
+            return res.status(500).send(err);
+        }
+    }else{
+        return res.status(403).json("You can Delete only your account")
+    }
+})
 //Get a User
+router.get("/:id", async (req, res)=>{
+    try{
+        const GetUser = await User.findById(req.params.id);
+        //will hide pasword and timestamps , spread them in other
+        const {password, updatedAt , ...PublicInfo} = GetUser._doc; 
+        res.status(200).json(PublicInfo);
+    }catch(err){
+        res.status(500).send(err);
+    }
+})
 //follow a User
+router.put("/:id/follow", async (req, res) => {
+    if(req.body.userId !== req.params.id){
+        try{
+            const UserToBeFollowed = await User.findById(req.params.id);
+            const CurrentUser = await User.findById(req.body.userId);
+            if(!UserToBeFollowed.followers.includes(req.body.userId)){
+                await UserToBeFollowed.updateOne({$push: {followers : req.body.userId}});
+                await CurrentUser.updateOne({$push: {followings : req.params.id}});
+                res.status(200).json("User has been followed successfully");
+            }else{
+                res.status(403).json("You already followed this user");
+            }
+        }catch(err){
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(403).json("You cannot follow yourself")
+    }
+})
 //Unfollow a User
 
 module.exports = router
