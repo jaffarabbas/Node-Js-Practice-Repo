1cb60ea0ff9f65d4b45965ac9aa5e1e341732af8
Jaffar Abbas <gamaportal8@gmail.com>
2023-04-06 20:17:58 +0500

    Commited File : index.js at 4/6/2023 8:17:58 PM

--- sql_test/index.js
+++ sql_test/index.js
@@ -1,47 +1,19 @@
-
+const { reset } = require('nodemon');
 var express = require('express');
-var sql = require("mssql/msnodesqlv8");
-// require("");
-var app = express();
+const dboperations = require('./dbOperation');
+const app = express();
+const router = new express.Router();
+app.use(express.json());
 
-app.get('/', function (req, res) {
-
-    // var sql = require("mssql");
-
-    // config for your database
-    var config = {
-        server: 'DESKTOP-44LIK56',
-        // user:'DARKLORD',
-        database: 'emp' ,
-        driver: "msnodesqlv8",
-        options: {
-            trustedConnection: true,
-        },
-        // trustServerCertificate: true,
-    };
-
-    // connect to your database
-    sql.connect(config, function (err) {
-        console.log('connected');
-
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-
-        // query to the database and get the records
-        request.query('select * from orders', function (err, recordset) {
-
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-
-        });
-    }).then(r => {
-
+router.get('/get', function (req, res) {
+    dboperations.getLoginDetails().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
     });
 });
+
+app.use(router);
 
 var server = app.listen(5000, function () {
     console.log('Server is running..');
