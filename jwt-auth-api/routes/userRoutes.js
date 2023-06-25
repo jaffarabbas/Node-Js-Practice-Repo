67167ec95e87ba4a4ed3f8f7d7636ac0ba3e7dbb
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-26 02:15:52 +0500

    Commited File : userRoutes.js at 26/06/2023 2:15:52 am

--- jwt-auth-api/routes/userRoutes.js
+++ jwt-auth-api/routes/userRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import UserController from '../controllers/userController.js';
+import checkToken from '../middlewares/auth-middleware.js';
 const router = express.Router();
+
+//route middleware
+router.use('/changePassword', checkToken);
+router.use('/loggedUser', checkToken);
 
 //public routes
 router.post('/register', UserController.userRegistration);
+router.post('/login', UserController.userLogin);
 
 //private routes
+router.post('/changePassword', UserController.changeUserPassword);
+router.get('/loggedUser', UserController.loggedInUser);
 
 export default router;
