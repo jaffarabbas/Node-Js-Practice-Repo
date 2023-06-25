67d6e8a9983a93f5b8da09b6d86380772055ff7c
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-25 14:23:57 +0500

    Commited File : app.js at 25/06/2023 2:23:57 pm

--- sm-api/app.js
+++ sm-api/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose, { connect } from "mongoose";
 import dotenv from "dotenv";
 import router from "./routes/user-routes";
+import blogRouter from "./routes/blog-router";
 //load env variables
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 //router middleware
 app.use('/api/users/',router);
+app.use('/api/blogs/',blogRouter);
 
 mongoose,connect(process.env.DB_KEY).then(() => {
     console.log("Connected to DB");
