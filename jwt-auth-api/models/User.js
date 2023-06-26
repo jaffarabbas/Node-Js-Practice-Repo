0fe6c5fba0154497d232c9fe537a2b0431fa8216
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-26 21:11:01 +0500

    Commited File : User.js at 26/06/2023 9:11:00 pm

--- jwt-auth-api/models/User.js
+++ jwt-auth-api/models/User.js
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         required: true,
     },
+    blogs: [{
+        type : mongoose.Types.ObjectId,
+        ref: "Blog",
+        required: true,
+    }]
 });
 
 const UserModel = mongoose.model('user', userSchema);
