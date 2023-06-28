1f5b1d4e059208cbecc51f419323d6c392c847f9
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-28 19:11:40 +0500

    Commited File : connectdb.js at 28/06/2023 7:11:40 pm

--- jwt-auth-api/config/connectdb.js
+++ jwt-auth-api/config/connectdb.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
 const connectDB =  async (DATABASE_URL) => {
     console.log('Connecting to database...');
     try{
-        const DB_OPTIONS = {
-            dbName: "authDB",
-        }
-        await mongoose.connect(DATABASE_URL, DB_OPTIONS);
+        await mongoose.connect(DATABASE_URL);
         console.log('Connected to database');
     }catch(error){
         console.log(error);
