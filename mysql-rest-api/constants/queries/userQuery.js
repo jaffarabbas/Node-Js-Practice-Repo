ac93dff87aa0649df1278316b72e2417506a98a2
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-01 12:16:43 +0500

    Commited File : userQuery.js at 01/07/2023 12:16:43 pm

--- mysql-rest-api/constants/queries/userQuery.js
+++ mysql-rest-api/constants/queries/userQuery.js
@@ -1,13 +1,3 @@
 class UserQuery{
-    static getInsertUserQuery(){
-        return "INSERT INTO users (name, email, password,user_type) VALUES (?, ?, ?, ?)";
-    }
-
-    static getSelectUserByEmailQuery(){
-        return "SELECT * FROM users WHERE email = ?";
-    }
-
-    static getSelectUserByIdQuery(){
-        return "SELECT * FROM users WHERE id = ?";
-    }
+    static getUsers = "INSERT INTO users (name, email, password,user_type) VALUES (?, ?, ?, ?)";
 }
