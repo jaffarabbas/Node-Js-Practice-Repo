9ad2269128bb2242b5311de79af93e4722e5b22c
Jaffar Abbas <gamaportal8@gmail.com>
2023-07-01 12:16:44 +0500

    Commited File : userService.js at 01/07/2023 12:16:44 pm

--- mysql-rest-api/services/userService.js
+++ mysql-rest-api/services/userService.js
@@ -2,6 +2,14 @@ import DbConnection from "../config/dbConfig.js";
 
 module.exports = {
     create: (data, callBack) => {
-        DbConnection.connection.query(UserQuery.getUsers)
+        DbConnection.connection.query(
+            UserQuery.getUsers,
+            [data.name, data.email, data.password, data.user_type],
+             (err, results, fields) => {  
+                if (err) {
+                    return callBack(err);
+                }
+                return callBack(null, results); 
+        });
     }
 }
