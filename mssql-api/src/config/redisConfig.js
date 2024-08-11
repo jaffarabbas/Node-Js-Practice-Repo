e7267b272aefcabad27e2f748d7136f24a2b67eb
jaffarabbas <gamaportal8@gmail.com>
2024-08-11 15:50:10 +0500

    node app with mssql server docker and compose

---
+++ mssql-api/src/config/redisConfig.js
@@ -0,0 +1,16 @@
+const { Redis } = require('ioredis')
+
+const redisClient = new Redis({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
+});
+
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis error', err);
+});
+
+module.exports = redisClient
---
+++ mssql-api/src/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const sql = require('mssql');
+const client = require('./config/redisConfig');
+const app = express()
+
+const port = process.env.PORT || 9000
+
+const config = {
+    user: 'sa',
+    password: 'JAFFARempire@1399',
+    server: 'mssql-server',
+    database: 'DB_JEWELSITE',
+    options: {
+        encrypt: false, // Use true for Azure SQL
+        trustServerCertificate: true, // Change to false if you are using a trusted certificate
+    },
+};
+
+app.get('/', async (req, res) => {
+    try {
+        // Connect to the database
+        await sql.connect(config);
+        const cacheData = await client.get("user:1")
+        if(cacheData) return res.json(JSON.parse(cacheData));
+        const result = await sql.query `SELECT * FROM USERS`;
+        await client.set("user:1",JSON.stringify(result.recordset))
+        await client.expire("user:1",100)
+        res.json(result.recordset);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error querying the database');
+    } finally {
+        // Close the connection
+        await sql.close();
+    }
+});
+
+
+app.listen(port, () => {
+    console.log('server is live!')
+})
